a4059df742ee793996ab49a1d9735b5756e7cf59
domagoj.vukovic <off.vukovic@gmail.com>
2023-02-18 18:19:05 +0100

    feat: lint setup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/** @type { import('@types/eslint').ESLint.ConfigData } */
+module.exports = {
+    extends: [require.resolve('@rimac-technology/style-guide/eslint/core')],
+    parser: '@typescript-eslint/parser',
+
+    parserOptions: {
+        project: './tsconfig.lint.json',
+    },
+}
---
+++ cspell.config.js
@@ -0,0 +1,7 @@
+/** @type { import('cspell').FileSettings } */
+const config = {
+    ignoreWords: ['domagoj', 'vukovic', 'bestend', 'logLevel', 'rimac'],
+    useGitignore: true,
+}
+
+module.exports = config
