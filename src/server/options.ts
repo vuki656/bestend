d83f8b25fb8752d83d1ffaf634895d080339e144
domagoj.vukovic <off.vukovic@gmail.com>
2023-02-19 12:11:29 +0100

    ref: remove unused file

--- src/server/options.ts
+++
@@ -1,8 +0,0 @@
-import type { startStandaloneServer } from '@apollo/server/standalone'
-
-import { context } from './context'
-
-export const options: Parameters<typeof startStandaloneServer>[1] = {
-    context,
-    listen: { port: 4000 },
-}
