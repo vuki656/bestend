6a44c5399de1547c0dc4934c568735ad09ccdc9a
domagoj.vukovic <off.vukovic@gmail.com>
2023-02-19 23:18:19 +0100

    feat: build with webpack wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
     extends: [require.resolve('@rimac-technology/style-guide/eslint/core')],
     ignorePatterns: [
         '*.generated.*',
+        'dist',
+        'node_modules',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
--- TODO.md
+++ TODO.md
@@ -1,9 +1,10 @@
 # NEXT
 
--   [ ] Login
+-   [ ] Build - not working because schema resolution path doesn't exist when built
 
 # BACKLOG
 
+-   [ ] Login
 -   [ ] Tests
 -   [ ] Pagination for lists (check github api) or something more simple ???
 -   [ ] AUTH
